227fdc67bf62226c167984fc75638d81b3279825
Wycka <vytska19@gmail.com>
2024-06-16 19:29:01 +0300

    feat: Add navigation button with icon to HistoryScreen

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen';
 import HistoryScreen from './src/screens/HistoryScreen';
 import { RootStackParamList } from './src/navigation/types';
+import { TouchableOpacity } from 'react-native';
+import Icon  from 'react-native-vector-icons/MaterialIcons';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
@@ -12,7 +14,17 @@ const App: React.FC = () => {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="History" component={HistoryScreen} />
+        <Stack.Screen 
+          name="History" 
+          component={HistoryScreen} 
+          options={({ navigation }) => ({
+            headerLeft: () => (
+              <TouchableOpacity onPress={() => navigation.goBack()} style={{ marginLeft: 10 }}>
+                <Icon name="arrow-back" size={30} color="#6200ee" />
+              </TouchableOpacity>
+            ),
+          })} 
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
